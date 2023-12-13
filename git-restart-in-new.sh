87af8f8b6c83191ac9c26ebe3742e3b11024594a
Adam <devops@gomro.cn>
2023-12-13 14:59:10 +0800

    init

---
+++ git-restart-in-new.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "正在拉取最新代码..."
+git fetch origin
+echo "拉取最新 main 分支..."
+git checkout -B main-bak origin/main
+echo "创建无历史分支..."
+git checkout --orphan latest_branch
+git add -A
+git commit -m "init"
+echo "本地初始化为 main..."
+git branch -D main
+git branch -m main
+echo "强制更新远程 main..."
+git push -f origin main
