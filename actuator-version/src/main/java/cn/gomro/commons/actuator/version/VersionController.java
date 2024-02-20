d679a7d7735045439d5398a74f03cc7fdafdee35
Adam <devops@gomro.cn>
2024-02-20 10:23:51 +0800

    update commons-dependencies 0.0.11 debug

--- actuator-version/src/main/java/cn/gomro/commons/actuator/version/VersionController.java
+++ actuator-version/src/main/java/cn/gomro/commons/actuator/version/VersionController.java
@@ -1,14 +1,41 @@
 package cn.gomro.commons.actuator.version;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *  暴露 /actuator/info 请先配置以下内容
+ *
+ * management:
+ *   endpoints:
+ *     web:
+ *       exposure:
+ *         include: health,info
+ *   info:
+ *     defaults:
+ *       enabled: true
+ *
+ * @since 2024/2/20 10:08
+ * @author Adam 2024/2/20 10:08 说明:
+ */
 @Slf4j
 @RestController
 @RequestMapping("/version")
-public class VersionController {
+public class VersionController implements InfoContributor {
+
+    @Value("${spring.profiles.active:not set}")
+    private String profile;
+
+    @Value("${spring.application.name:not set}")
+    private String applicationName;
+
+    @Value("${spring.cloud.client.hostname:not set}")
+    private String cloudClientHostname;
 
     @GetMapping("")
     public String version() {
@@ -18,5 +45,13 @@ public class VersionController {
         }
         log.debug("appVersion is {}", appVersion);
         return appVersion;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+
+        builder.withDetail("profile", profile)
+                .withDetail("application", applicationName)
+                .withDetail("hostname", cloudClientHostname);
     }
 }
